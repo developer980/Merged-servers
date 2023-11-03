f16fa2d3813a850d5800e0c47416dbe4229ae52f
developer980 <tudordin2002@gmail.com>
2023-11-03 18:22:39 +0200

    minor fixes

--- Models/User.js
+++ Models/User.js
@@ -10,7 +10,12 @@ class User{
         this.email = email;
         this.username = username
     }
+
+    
+    addUser(param1, param2) {
+        this.param1 = param1;
+        this.param2 = param2
+    }
 }
- //change 
 
 module.exports = User;
