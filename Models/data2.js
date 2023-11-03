a744348e08798ae7b9ea0d430fd05ad444aef05b
developer980 <tudordin2002@gmail.com>
2023-11-03 18:42:23 +0200

    test

---
+++ Models/data2.js
@@ -0,0 +1,13 @@
+class Data1{
+    constructor(info1, info2) {
+        this.info1 = info1;
+        this.info2 = info2;
+    }
+
+    setInfo(info1, info2) {
+        this.info1 = info1;
+        this.info2 = info2;
+    }
+}
+
+module.exports = Data1;
